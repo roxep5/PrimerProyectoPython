b7adc3f39eb5c39c107fe7cb2a6d75817bb11d82
roxep5 <58743609+roxep5@users.noreply.github.com>
2020-10-23 14:20:30 +0200

    Create Clients.py

---
+++ proyecto2/Clients.py
@@ -0,0 +1,17 @@
+class Clientes();
+    def validarDNI(dni):
+        tabla=
+        dig_ext='XYZ'
+        reemp_dig_ext={'X':'0','Y':'1','Z':'2'}
+        numeros='123456789'
+        dni=dni.upper()
+        if(len(dni)==9):
+                dig_control=dni[8]
+                dni=dni[:8]
+                if(dni[0]in dig_ext):
+                    dni=dni.replace(dni[0],reemp_dig_ext[dni[0]])
+                return len(dni==len([n for n in dni if n in numeros])) and tabla(int(dni)%23)==dig_control
+        return False
+    except:
+        print('Errpr en el modulo de validacion del DNI')
+        return None
