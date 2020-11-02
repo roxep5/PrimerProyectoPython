2714034f9802eec7215f43f82a607da461d85fff
Roxep5 <pinhayop@gmail.com>
2020-11-02 19:04:19 +0100

    empezamos de cero

--- proyecto2/main.py
+++ proyecto2/main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from proyecto2.Ventana import *
+import sys
 
+class Main(QtWidgets.QMainWindow):
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        def __init__(self):
+            super(Main, self).__init__()
+            self.ui=Ui_MainWindow()
+            self.ui.setupUi(self)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__=='__main__':
+    app=QtWidgets.QApplication([])
+    window=Main()
+    window.show()
+    sys.exit(app.exec())
